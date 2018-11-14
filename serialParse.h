b4ecb8c0b7e27c2a5b14be8f737dad57b8b98921
Spencer Graffunder <spencergraffunder@cedarforest.cedarville.edu>
2018-11-14 13:23:52 -0500

    add some parsing

--- serialParse.h
+++ serialParse.h
@@ -15,14 +15,12 @@ void parse(String str){
     String command = str.substring(0, firstSpaceIndex);
     // take off first word
     str = str.substring(firstSpaceIndex + 1);
-	
-    
 
     if(command.equals("set")) {
         int secondSpaceIndex = str.indexOf(' ');
         String name = str.substring(0, secondSpaceIndex);
         str = str.substring(secondSpaceIndex + 1);
-
+        float val = str.toFloat();
         // Serial.print("set");
         // Serial.println(name);
 
@@ -30,16 +28,29 @@ void parse(String str){
         // Serial3.println(name);
 		
         if(name.equals("rp")){
-            float val = str.toFloat();
-            Serial.print("val = ");
-            Serial.println(val);
+            // Serial.print("val = ");
+            // Serial.println(val);
 			
-            Serial3.print("val = ");
-            Serial3.println(val);
+            // Serial3.print("val = ");
+            // Serial3.println(val);
         }else if(name.equals("ri")){
 
+        }else if(name.equals("rd")){
+
+        }else if(name.equals("pp")){
+
+        }else if(name.equals("pi")){
+
+        }else if(name.equals("pd")){
+
+        }else if(name.equals("yp")){
+
+        }else if(name.equals("yi")){
+
+        }else if(name.equals("yd")){
+
         }else if(name.equals("ga")){
-            float val = str.toFloat();
+
             SpecMPU6050::setGA(val);
         }
     }else if(command.equals("save")){
